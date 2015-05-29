a1907adda7f1bf4f59a0c7e059a125a3199d41b4
Max Kharandziuk <max.kharandziuk@gmail.com>
2015-05-30 01:18:55 +0300

    ability to handle file path

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var
   assert = require('assert'),
   _ = require('underscore'),
   debug = require('debug'),
-  lib = require('./lib');
+  lib = require('./lib'),
+  filePath = process.argv[2];
 
 
 /*
@@ -17,11 +18,18 @@ var
 */
 
 
-var lineStream = fs.createReadStream('data-samples/sample.log');
+
+var lineStream = fs.createReadStream(filePath);
+
 
 //2014-01-09T06:18:04.947113+00:00 heroku[router]: at=info method=POST path=/version_api/files host=services.pocketplaylab.com fwd="81.152.126.250" dyno=web.13 connect=1ms service=35ms status=200 bytes=69
-
-H(lineStream).split()
+  H(lineStream).errors(function(err){
+    if(err.code == 'ENOENT') {
+      console.log('No such file', err.path);
+    } else {
+    throw err;
+    }
+  }).split()
   .map(lib.parseLine)
   .map(function(obj) {
     obj.query = lib.determineQuery(obj.path, obj.method);
